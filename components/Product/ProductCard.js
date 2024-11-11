cdb244900becb832503bfbc43768ac414d912cc1
Parker Maughan <parkermaughan@gmail.com>
2024-11-11 17:08:52 -0500

    Refactor product options filter in shop.js

--- components/Product/ProductCard.js
+++ components/Product/ProductCard.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { formatPrice } from '@/utils/index'
-import styles from './ProductCard.module.css' // Import custom styles
+import styles from './ProductCard.module.css' // Assuming you have a CSS module file
 
 function ProductCard({ product }) {
-  const { handle, title, images, tags } = product
-  const [mainImage, setMainImage] = useState(images?.edges?.[0]?.node)
-  const price = parseFloat(product.priceRange.minVariantPrice.amount)
-  const originalPrice = product.compareAtPriceRange?.maxVariantPrice?.amount
-    ? parseFloat(product.compareAtPriceRange.maxVariantPrice.amount)
+  const { handle, title, images, tags, priceRange, compareAtPriceRange } =
+    product
+  const [featuredImage, setFeaturedImage] = useState(images?.edges?.[0]?.node)
+  const price = formatPrice(priceRange.minVariantPrice.amount)
+  const originalPrice = compareAtPriceRange?.minVariantPrice?.amount
+    ? formatPrice(compareAtPriceRange.minVariantPrice.amount)
     : null
-  const savingsPercentage =
-    originalPrice && originalPrice > price
-      ? Math.round(((originalPrice - price) / originalPrice) * 100)
-      : null
-
-  // Log the product data to verify
-  console.log('Product data:', product)
+  const hasSale =
+    originalPrice &&
+    compareAtPriceRange.minVariantPrice.amount >
+      priceRange.minVariantPrice.amount
+  const savings = hasSale
+    ? (
+        ((compareAtPriceRange.minVariantPrice.amount -
+          priceRange.minVariantPrice.amount) /
+          compareAtPriceRange.minVariantPrice.amount) *
+        100
+      ).toFixed(2)
+    : null
 
   return (
     <Link href={`/products/${handle}`}>
       <a className="group">
-        <div className="border">
-          <div className="relative w-full overflow-hidden aspect-w-1 aspect-h-1 md:aspect-w-1 md:aspect-h-1">
-            {mainImage && (
+        <div className="bg-gray-100 border">
+          <div
+            className={`relative w-full overflow-hidden bg-gray-400 aspect-w-1 aspect-h-1 md:aspect-w-2 md:aspect-h-3 ${styles.productImage}`}
+          >
+            {featuredImage && (
               <Image
-                src={mainImage.transformedSrc}
-                alt={mainImage.altText ?? 'Product Image'}
+                src={featuredImage.transformedSrc}
+                alt={featuredImage.altText ?? 'Product Image'}
                 className="object-cover object-center w-full h-full group-hover:opacity-75"
                 layout="fill"
                 priority
@@ -42,28 +50,24 @@ function ProductCard({ product }) {
               {tags?.[0] ?? 'No tag'}
             </p>
             <div className="flex items-center mt-1">
-              <p className="text-sm text-gray-700">{formatPrice(price)}</p>
-              {originalPrice && originalPrice > price && (
-                <p className="ml-2 text-sm text-gray-500 line-through">
-                  {formatPrice(originalPrice)}
-                </p>
+              <p className="text-sm text-gray-700">{price}</p>
+              {hasSale && (
+                <>
+                  <p className="ml-2 text-sm text-gray-500 line-through">
+                    {originalPrice}
+                  </p>
+                  <p className="ml-2 text-sm text-green-500">{savings}% off</p>
+                </>
               )}
             </div>
-            {savingsPercentage && (
-              <p className="mt-1 text-sm text-green-500">
-                {savingsPercentage}% off
-              </p>
-            )}
           </div>
-          <div
-            className={`flex justify-center mt-2 space-x-2 ${styles.thumbnailContainer}`}
-          >
-            {images?.edges?.slice(1).map(({ node }) => (
+          <div className="flex justify-center mt-2 space-x-2">
+            {images?.edges?.slice(1, 4)?.map(({ node }, index) => (
               <div
-                key={node.id}
-                className={`relative w-16 h-16 overflow-hidden ${styles.thumbnail}`}
-                onMouseEnter={() => setMainImage(node)}
-                onMouseLeave={() => setMainImage(images?.edges?.[0]?.node)}
+                key={`${node.id}-${index}`} // Ensure each key is unique
+                className="relative w-12 h-12 overflow-hidden bg-gray-200"
+                onMouseEnter={() => setFeaturedImage(node)}
+                onMouseLeave={() => setFeaturedImage(images?.edges?.[0]?.node)}
               >
                 <Image
                   src={node.transformedSrc}
